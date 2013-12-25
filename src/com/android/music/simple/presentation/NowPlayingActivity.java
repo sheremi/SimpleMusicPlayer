d5016c7d5e36cc220650528e06e8ab92582dbb8f
Kate <kate.lyavinskova@gmail.com>
2013-12-25 14:30:57 +0100

    Added first view to the activity

--- src/com/android/music/simple/presentation/NowPlayingActivity.java
+++ src/com/android/music/simple/presentation/NowPlayingActivity.java
@@ -2,9 +2,12 @@ package com.android.music.simple.presentation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.TextView;
 
 import com.android.music.R;
 import com.android.music.simple.events.PlaypositionChanged;
+import com.android.music.simple.events.TrackInfoChanged;
 import com.android.music.simple.model.IPlayer;
 import com.android.music.simple.model.PlayerService;
 import com.github.androidutils.eventbus.EventBusBroadcastReceiver;
@@ -14,13 +17,15 @@ import com.google.common.eventbus.Subscribe;
 public class NowPlayingActivity extends Activity {
     EventBusBroadcastReceiver eventBusBroadcastReceiver;
     private Logger logger;
+    private TextView mArtistName;
+    private IPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         logger = Logger.getDefaultLogger();
-        setContentView(R.layout.audio_player);
-        IPlayer player = PlayerService.getPlayer();
+        setContentView(R.layout.simple_now_playing_activity);
+        player = PlayerService.getPlayer();
         eventBusBroadcastReceiver = new EventBusBroadcastReceiver();
         eventBusBroadcastReceiver.register(this, this);
     }
@@ -28,5 +33,14 @@ public class NowPlayingActivity extends Activity {
     @Subscribe
     public void handle(PlaypositionChanged event) {
         logger.d(event.toString());
+    }
+
+    @Subscribe
+    public void handle(TrackInfoChanged event) {
+        String artistName = event.artistName;
+        if (MediaStore.UNKNOWN_STRING.equals(artistName)) {
+            artistName = getString(R.string.unknown_artist_name);
+        }
+        mArtistName.setText(artistName);
     }
 }
